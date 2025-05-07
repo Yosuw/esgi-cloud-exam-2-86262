4b94914e37ba355242c826243c6529bc2d099c10
Yosu <yosuu.pro@gmail.com>
2025-05-07 12:39:22 +0200

    2nd commit

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DATABASE_URL || 'postgres://fakeurl', // Utilisation de la variable d'environnement
   {
     dialectOptions: {
       ssl: {
@@ -16,8 +16,9 @@ const sequelize = new Sequelize(
 // authentication and synchronization
 sequelize.authenticate()
   .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
+    console.log("Connection to the database has been established successfully.");
+    sequelize.sync().catch((error) => console.log("Cannot sync the database:", error));
   })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+  .catch((error) => console.log("Cannot connect to database, please check environment credentials:", error));
 
 module.exports = sequelize;
